6bbd26e904c134d776512702ab7e7c4adf2d4a37
PaytanDouglas <douglap@clarkson.edu>
2019-10-22 10:19:38 -0400

    edited the lorem ipsum to be more realistic

--- js/page.js
+++ js/page.js
@@ -1,1 +1,11 @@
 //* Page Scripts
+//Mobile navigation Toggle
+function navToggle() {
+  var x = document.getElementById("main-nav");  //creates a variable set to #main-nav
+  if (x.className === "topnav") {               //if #main-nav has a class of .topnav
+    x.className += " responsive";               //then add a new class
+  }
+  else {                                        //Otherwise
+    x.className = "topnav";                     //set class to .topnav
+  }
+}
