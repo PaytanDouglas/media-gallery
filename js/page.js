76133ff884865c56ce81a98f77e5191660eb4ea2
PaytanDouglas <douglap@clarkson.edu>
2019-10-22 10:38:42 -0400

    added code from today's lecture ; edited more of the dialogue for cards ; organized code

--- js/page.js
+++ js/page.js
@@ -1,11 +1,10 @@
 //* Page Scripts
-//Mobile navigation Toggle
-function navToggle() {
-  var x = document.getElementById("main-nav");  //creates a variable set to #main-nav
-  if (x.className === "topnav") {               //if #main-nav has a class of .topnav
-    x.className += " responsive";               //then add a new class
-  }
-  else {                                        //Otherwise
-    x.className = "topnav";                     //set class to .topnav
-  }
-}
+// Mobile Navigaton Toggle
+function navToggle() {                            // creates a new function named navToggle
+  var x = document.getElementById("main-nav");    // creates a variable set to #main-nav
+    if (x.className === "topnav") {               // if #main-nav has a class of .topnav,
+    x.className += " responsive";                 // then add a new class
+  } else {                                        // if it doesn't,
+    x.className = "topnav";                       // set class to .topnav
+  }                             //closes if-else
+}                               //closes function
