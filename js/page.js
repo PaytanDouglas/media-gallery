7691080f551133873991aebb13e3d79632fae21c
PaytanDouglas <douglap@clarkson.edu>
2019-10-24 11:33:13 -0400

    added modal and slideshow aspects ; added color scheme (doesn't work for some reason) ;

--- js/page.js
+++ js/page.js
@@ -6,5 +6,43 @@ function navToggle() {                            // creates a new function name
     x.className += " responsive";                 // then add a new class
   } else {                                        // if it doesn't,
     x.className = "topnav";                       // set class to .topnav
-  }                             //closes if-else
-}                               //closes function
+  }
+}
+
+//Slideshow function
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// Image modal functions
+$(".button").on("click", function() {
+  var modal = $(this).data("modal"); // sets modal var equal to data attribute
+  $(modal).show();
+});
+$(".modal").on("click", function(e) {
+  var className = e.target.className; // var className set to event target
+  if(className === "modal" || className === "close"){
+    $(this).closest(".modal").hide(); // then hide the modal
+  }
+});
