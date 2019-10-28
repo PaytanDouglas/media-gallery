cff9acfe6e9583661bd4db87daecc68e88598c61
PaytanDouglas <douglap@clarkson.edu>
2019-10-28 11:41:39 -0400

    undid the damage done

--- js/page.js
+++ js/page.js
@@ -35,14 +35,15 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+
 // Image modal functions
-$(".button").on("click", function() {
+$(".button").on("click", function() { // when item with class of button is clicked, fire function
   var modal = $(this).data("modal"); // sets modal var equal to data attribute
-  $(modal).show();
+  $(modal).show(); // opens up modal (much code hidden here by jQuery)
 });
-$(".modal").on("click", function(e) {
+$(".modal").on("click", function(e) { // sets up click function
   var className = e.target.className; // var className set to event target
-  if(className === "modal" || className === "close"){
+  if(className === "modal" || className === "close"){ // if conditions are met...
     $(this).closest(".modal").hide(); // then hide the modal
-  }
-});
+  } // closes if-statement
+}); // closes function
